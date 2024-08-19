9beba0a78ca30b11d725ad2c62a6b5ca1fbaf846
huy le <ledanhhuy20192910@gmail.com>
2024-08-19 10:47:04 +0700

    khong tham so truyen vao

--- B3_Pointer/Motor.c
+++ B3_Pointer/Motor.c
@@ -1,49 +1,60 @@
 #include <stdio.h>
 
-typedef struct {
-   void (*start)(int gpio);       // con trỏ hàm start 
-   void (*stop)(int gpio);        // con trỏ hàm stop 
-   void (*changeSpeed)(int gpio, int speed);    // con trỏ hàm change speed  
-} MotorController;
-
 typedef int PIN;
 
+typedef struct {
+   PIN pinName;           // chân điều khiển
+   void (*start)();       // con trỏ hàm start 
+   void (*stop)();        // con trỏ hàm stop 
+   void (*changeSpeed)(int speed);    // con trỏ hàm change speed  
+} MotorController;
+
+
+// khai báo 1 con trỏ motorCurrent 
+MotorController *motorCurrent = NULL;
+
 // Các hàm chung
-void startMotor(PIN pin) {
-   printf("Start motor at PIN %d\n", pin);
+void startMotor() {
+   printf("Start motor at PIN %d\n", motorCurrent -> pinName);
 }
 
-void stopMotor(PIN pin) {
-   printf("Stop motor at PIN %d\n", pin);
+void stopMotor() {
+   printf("Stop motor at PIN %d\n", motorCurrent -> pinName);
 }
 
-void changeSpeedMotor(PIN pin, int speed) {
-   printf("Change speed at PIN %d: %d\n", pin, speed);
+void changeSpeedMotor(int speed) {
+   printf("Change speed at PIN %d: %d\n", motorCurrent -> pinName, speed);
 }
 
 // Macro để khởi tạo GPIO và MotorController
 // ví dụ nếu truyên vào INIT_MOTOR(mA, 3) 
 // => pin_mA = 3; mA = {startMotor, stopMotor, changeSpeedMotor};
 // khởi tạo giá trị của 3 trường tương ứng với địa chỉ của 3 hàm 
-#define INIT_MOTOR(motorName, pinNumber) \        
-   PIN PIN_##motorName = pinNumber; \
-   MotorController motorName = {startMotor, stopMotor, changeSpeedMotor};
 
+#define INIT_MOTOR(motorName, pinNumber)  \
+   PIN PIN_##motorName = pinNumber;       \
+   MotorController motorName = {PIN_##motorName, startMotor, stopMotor, changeSpeedMotor};
+
+#define SET_MOTOR_CURRENT(MotorController)\
+   motorCurrent = &MotorController; 
 
 int main() {
+
    // Sử dụng macro để khởi tạo
    INIT_MOTOR(motorA, 1);
    INIT_MOTOR(motorB, 2);
 
    // Sử dụng motorA
-   motorA.start(PIN_motorA);
-   motorA.changeSpeed(PIN_motorA, 50);
-   motorA.stop(PIN_motorA);
+   SET_MOTOR_CURRENT(motorA);
+   motorA.start();
+   motorA.changeSpeed( 50);
+   motorA.stop();
 
    // Sử dụng motorB
-   motorB.start(PIN_motorB);
-   motorB.changeSpeed(PIN_motorB, 75);
-   motorB.stop(PIN_motorB);
+   SET_MOTOR_CURRENT(motorB);
+   motorB.start();
+   motorB.changeSpeed(75);
+   motorB.stop();
 
    return 0;
 }
